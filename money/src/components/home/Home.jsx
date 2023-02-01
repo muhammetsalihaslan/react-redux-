de1919172cc2422b9ae2d13c84085ad5af90b93a
salih <muhammetsalihaslan@gmail.com>
2023-02-01 11:56:44 +0300

    redux example starting

---
+++ money/src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <img
+          className="navbarImg"
+          src="https://neal.fun/spend/billgates.jpg"
+          alt=""
+        />
+        <h2>Spend Bill Gates' Money</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
