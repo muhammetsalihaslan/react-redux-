4f8e4052514b001bcfd71ad402ac60074e0212a4
salih <muhammetsalihaslan@gmail.com>
2023-02-05 17:33:14 +0300

    slice

--- money/src/redux/shopSlice.js
+++ money/src/redux/shopSlice.js
@@ -4,9 +4,14 @@ export const shopSlice = createSlice({
   name: "shopping",
   initialState: {
     items: [],
+    money: 1000000000,
   },
 
   reducers: {},
 });
 
+export const money = state.
+
 export default shopSlice.reducer;
+
+
